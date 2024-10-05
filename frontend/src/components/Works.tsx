d3ad8a396a1af851e4385c3a539434fe7004a095
fumi <fumi@example.com>
2024-10-05 14:15:09 +0900

    APIでバックエンドからworks取得

--- frontend/src/components/Works.tsx
+++ frontend/src/components/Works.tsx
@@ -1,36 +1,36 @@
-import { useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query';
 import { Layout } from './Layout';
 import { API_PATH } from '../ApiConfig';
 
 type JobType = {
-  year: number;
+  title: string;
   description: string;
+  engaged_at: string;
 };
-const Jobs: JobType[] = [
-  { year: 2000, description: 'aaa' },
-  { year: 2001, description: 'bbb' },
-  { year: 2002, description: 'ccc' },
-  { year: 2003, description: 'ddd' },
-];
 
 export const Works = () => {
   const Title = 'Works';
 
   const fetchWorks = async () => {
-    await fetch(API_PATH.works).then((res) => res.json());
+    return await fetch(API_PATH.works).then((res) => {
+      return res?.json();
+    });
   };
 
-  const query = useQuery({ queryKey: ['works'], queryFn: fetchWorks });
-
-  // console.log(query.data);
+  const { data: jobs } = useQuery({
+    queryKey: ['jobs'],
+    queryFn: fetchWorks,
+  });
 
   return (
     <Layout title={Title}>
       <ul>
-        {Jobs.map((job: JobType) => {
+        {jobs?.map((job: JobType, idx: number) => {
           return (
-            <li>
-              {job.year}:{job.description}
+            <li key={idx}>
+              {job.engaged_at}
+              {job.title}
+              {job.description}
             </li>
           );
         })}

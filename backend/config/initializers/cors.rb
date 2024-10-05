5058d3dd7f6026cd9952ed6bdc8e83a4401bb07f
fumi <fumi@example.com>
2024-10-05 14:28:04 +0900

    bundle exec rubocop

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -9,10 +9,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8000"
+    origins 'localhost:8000'
 
-    resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*',
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
